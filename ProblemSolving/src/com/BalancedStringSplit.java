2f6559d8856aadd059dcb9a37ce8ef007ac0778a
kcherianthomas <57378953+kcherianthomas@users.noreply.github.com>
2019-12-29 22:51:26 +0300

    faster code

--- ProblemSolving/src/com/BalancedStringSplit.java
+++ ProblemSolving/src/com/BalancedStringSplit.java
@@ -10,31 +10,16 @@ import java.util.Stack;
 public class BalancedStringSplit {
 	public int balancedStringSplit(String s) {
 		int count = 0;
-		char top = 'n';
-		Stack<Character> stack = new Stack<Character>();
+		int lcount=0;
+		int rcount=0;
 		for (int i = 0; i < s.length(); ++i) {
-			if ((s.charAt(i) == 'L' && stack.isEmpty())) {
-				stack.push('L');
-				top = 'L';
-			} else if ((s.charAt(i) == 'R') && stack.isEmpty()) {
-				stack.push('R');
-				top = 'R';
-			} else if (top == 'L' && s.charAt(i) == 'L' && !stack.isEmpty()) {
-				stack.push('L');
-			} else if (top == 'R' && s.charAt(i) == 'R' && !stack.isEmpty()) {
-				stack.push('R');
-			} else if (top == 'L' && s.charAt(i) == 'R' && !stack.isEmpty()) {
-				stack.pop();
-				if (stack.isEmpty()) {
-					top = 'n';
-					count++;
-				}
-			} else if (top == 'R' && s.charAt(i) == 'L' && !stack.isEmpty()) {
-				stack.pop();
-				if (stack.isEmpty()) {
-					top = 'n';
-					count++;
-				}
+			if (s.charAt(i) == 'L') {
+				lcount++;
+			} else if (s.charAt(i) == 'R')  {
+				rcount++;
+			} 
+			if((s.charAt(i) == 'R' || s.charAt(i) == 'L') && lcount==rcount) {
+				count++;
 			}
 		}
 		return count;
