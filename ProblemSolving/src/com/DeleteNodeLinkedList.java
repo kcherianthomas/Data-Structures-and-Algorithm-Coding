52491b11b967602f817a8e0c2865a755c6b9503e
kcherianthomas <57378953+kcherianthomas@users.noreply.github.com>
2020-01-04 00:01:35 +0300

    The question was simpler than i thought

--- ProblemSolving/src/com/DeleteNodeLinkedList.java
+++ ProblemSolving/src/com/DeleteNodeLinkedList.java
@@ -7,14 +7,7 @@ package com;
  * The given node will not be the tail and it will always be a valid node of the linked list.
  */
 public class DeleteNodeLinkedList {
-	public void deleteNode(ListNode node) {
-		while (null != node.next) {
-			node.val = node.next.val;
-			if (node.next.next == null) {
-				node.next = null;
-			} else {
-				node = node.next;
-			}
-		}
-	}
+	public void deleteNode(ListNode node) {node.val=node.next.val;
+    node.next=node.next.next;
+    }
 }
