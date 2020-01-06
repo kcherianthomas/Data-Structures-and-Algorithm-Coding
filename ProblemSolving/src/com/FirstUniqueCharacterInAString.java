c875d13d6f11b4e3153a476a0314a5cb5c704ecf
kcherianthomas <57378953+kcherianthomas@users.noreply.github.com>
2020-01-06 20:06:21 +0300

    optimized further

--- ProblemSolving/src/com/FirstUniqueCharacterInAString.java
+++ ProblemSolving/src/com/FirstUniqueCharacterInAString.java
@@ -1,30 +1,28 @@
 package com;
 
-import java.util.HashMap;
-import java.util.Map;
 /*
  * Given a string, find the first non-repeating character in it and return it's index. 
  * If it doesn't exist, return -1.
  */
 public class FirstUniqueCharacterInAString {
 	/*
-	 * Uses the hashmap to save frequency and
-	 * Iterate again to find first unique
+	 * Slightly modified the previous approach to use
+	 * an array[26] instead of hashmap as its told
+	 * the string contains only lowercase letters
 	 */
 	public int firstUniqChar(String s) {
 		if(null==s || s.equals("")) {
 			return -1;
 		}
-		Map<Character,Integer> m = new HashMap<>();
-		for(int i=0;i<s.length();++i) {
-		if(!m.containsKey(s.charAt(i))) {
-		m.put(s.charAt(i), 1);
-		} else {
-			m.put(s.charAt(i), m.get(s.charAt(i))+1);
-		}
+		int [] arr = new int[26];
+		for(int i=0;i<26;++i) {
+			arr[i]=0;
 		}
 		for(int i=0;i<s.length();++i) {
-			if(m.get(s.charAt(i))==1) {
+			arr[s.charAt(i)-'a'] = arr[s.charAt(i)-'a']+1;
+		}
+		for(int i=0;i<s.length();++i) {
+			if(arr[s.charAt(i)-'a']==1) {
 				return i;
 			}
 		}
