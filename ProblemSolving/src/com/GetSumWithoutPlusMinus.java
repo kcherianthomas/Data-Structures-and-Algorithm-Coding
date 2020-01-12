263bb8334dc1f7a61ab735ddd899571a45adc1cd
kcherianthomas <57378953+kcherianthomas@users.noreply.github.com>
2020-01-12 15:31:53 +0300

    adding two numbers without using + or - commented

--- ProblemSolving/src/com/GetSumWithoutPlusMinus.java
+++ ProblemSolving/src/com/GetSumWithoutPlusMinus.java
@@ -1,17 +1,23 @@
 package com;
 
+/*
+ * The sum of two integers a and b, without using the operator + and -.
+ */
 public class GetSumWithoutPlusMinus {
 	public int getSum(int a, int b) {
-        int carry=0;
-        int sum=0;
-        carry=(a^b);
-        sum = a&b;
-        int total = (sum<<1*(log2(carry)))|(carry);
-        return total;
-    }
-	
-	public int log2(int x)
-	{
-	    return (int) (Math.log(x) / Math.log(2));
+		//The loop iterates until all carries are cleared
+		while (b != 0) {
+			int carry = a & b;
+			/*xor helps to simulate binary addition
+			 * for example if we want to add 1+1 we know units place is zero
+			 */
+			a = a ^ b;
+			/* we need to apply carry 1 position left
+			 * for example when we add 1+1 the carry needs to be applied left of
+			 * units place to get 10
+			 */
+			b = carry << 1;
+		}
+		return a;
 	}
 }
